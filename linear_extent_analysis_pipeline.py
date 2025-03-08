1e2addad9ec2ebb82ffac35b8922c6f42048e9ea
graymckenna96 <graymckenna96@gmail.com>
2025-03-07 18:26:29 -0800

    modified to run latest versions of scripts

--- linear_extent_analysis_pipeline.py
+++ linear_extent_analysis_pipeline.py
@@ -4,20 +4,24 @@ import subprocess
 
 # All data sources should be copied into /kelp_data_sources folder
 
-# Historical datasources, no updates anticipated
-# No need to rerun unless datasets have been editted since 06/2024
+# Historical/"one time" datasources, no updates anticipated
+# No need to rerun unless datasets have been editted since 03/2025
 subprocess.run(["python", "COSTR_synth.py"])
 subprocess.run(["python", "AQRES_synth.py"])
-subprocess.run(["python", "cps_sps_synth.py"])
-subprocess.run(["python", "samish_synth.py"])
+subprocess.run(["python", "cps_sps_reformat.py"])
+#subprocess.run(["python", "samish_synth.py"])
+subprocess.run(["python", "cps_uas_synth.py"])
+subprocess.run(["python", "shorezone_synth.py"])
 
 # Living datasources
 # Rerun as updates occur 
-subprocess.run(["python", "kayak_synth.py"]) # Annual updates anticipated
-subprocess.run(["python", "fixedwing_synth.py"]) # Annual updates anticipated
+subprocess.run(["python", "DNR_kayak_synth.py"]) # Annual updates anticipated
+subprocess.run(["python", "fixedwing_poly_synth.py"]) # Annual updates anticipated
+subprocess.run(["python", "mrc_kayak_synth.py"])
+
 
 # Run the join script
-subprocess.run(["python", "kelp_synth_join.py"])
+#subprocess.run(["python", "kelp_synth_join.py"])
 
 # Linear Extent updated
 print("Analysis complete")
