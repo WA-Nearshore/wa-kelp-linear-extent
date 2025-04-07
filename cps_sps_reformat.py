548e1db67bf92f04375b66b79f39edc8c042f5e4
graymckenna96 <graymckenna96@gmail.com>
2025-04-07 14:25:16 -0700

    modify to use new abundance line calc function

--- cps_sps_reformat.py
+++ cps_sps_reformat.py
@@ -1,25 +1,31 @@
 # Reformat the CPS & SPS linear extent field survey data
 
+# set env -----------------------------------------------------------
 import arcpy
+import sys
 import pandas as pd
 import os
 from arcgis.features import GeoAccessor, GeoSeriesAccessor
-
+import fns
+sys.path.append(os.getcwd())
 arcpy.env.overwriteOutput = True
 
-def reset_ws(): 
-    arcpy.env.workspace = os.getcwd()
-
-reset_ws()
+fns.reset_ws()
 
 # prep data ------------------------------------------------------------
 
 # cps data from K:\Kelp\2019_cps_field\spatial_data\bull_kelp_cps_2019.gdb
 cps_orig= "kelp_data_sources\\bull_kelp_cps_2019.gdb\\bull_kelp_2019"
+# consider switching this to fringe at some point 
 
 # sps data from K:\kelp\projects\historical_comparison_sps_2018\spatial_data\SS_kelp.gdb
 sps_orig = "kelp_data_sources\\SS_kelp.gdb\\dnr2017_ss"
 
+print(f"Running analsysis on {cps_orig} and {sps_orig}...")
+# abundance containers
+abundance_containers = "LinearExtent.gdb\\abundance_containers"
+
+print("Copying features to scratch.gdb...")
 # copy datasets to scratch
 cps_fc = "scratch.gdb\\cps"
 sps_fc = "scratch.gdb\\sps"
@@ -27,49 +33,41 @@ sps_fc = "scratch.gdb\\sps"
 arcpy.management.CopyFeatures(cps_orig, cps_fc)
 arcpy.management.CopyFeatures(sps_orig, sps_fc)
 
-# calculate length field 
-arcpy.management.CalculateField(cps_fc, "length", "!SHAPE_LENGTH!")
-arcpy.management.CalculateField(sps_fc, "length", "!SHAPE_LENGTH!")
-
 # convert to df 
+print("Converting to dataframes...")
 cps_df = pd.DataFrame.spatial.from_featureclass(cps_fc)
 sps_df = pd.DataFrame.spatial.from_featureclass(sps_fc)
 
-# calculate sps resence and abundance -----------------------------------
-
+# calculate sps presence and abundance -----------------------------------
+print("Processing SPS data...")
+print("Calculating presence...")
 # presence
 sps_pres = sps_df.groupby('SITE_CODE', as_index=False).agg(
     {'kelp':'max'}
 ).rename(columns={'kelp': 'presence'})
 sps_pres['year'] = '2017'
 sps_pres['source'] = 'WADNR_sps_boat_survey'
+print("Presence results: ")
+print(sps_pres.head())
 
 # abundance
-# calculate total_length for each SITE_CODE
-sps_df['length'] = pd.to_numeric(sps_df['length'])
-sps_df['total_length'] = sps_df.groupby('SITE_CODE')['length'].transform('sum')
+# create a filtered version of the dataset with only line segments w/ kelp present 
+print("Filteritng dataset to presence features only...")
+sps_df_filt = sps_df[sps_df['kelp']== 1]
+sps_fc_filt = "scratch.gdb\\sps_kelp_only"
+sps_df_filt.spatial.to_featureclass(location=sps_fc_filt, overwrite=True)
 
-# calculate weight of each section
-sps_df['weight'] = sps_df['length'] / sps_df['total_length']
-
-# get weighted presence for each section
-sps_df['w_pres'] = sps_df['weight'] * sps_df['kelp']
-
-# sum weighed presence by SITE_CODE
-sps_ab = (sps_df.groupby('SITE_CODE')
-                .agg(sum_w_pres=('w_pres', 'sum'))
-                .reset_index())
-
-# convert to abundance
-sps_ab['abundance'] = pd.cut(sps_ab['sum_w_pres'],
-                                    bins=[-float('inf'), 0, 0.25, 0.5, 0.75, float('inf')],
-                                    labels=[0, 1, 2, 3, 4])
-
-# drop extra col
-sps_ab = sps_ab.drop(columns=['sum_w_pres'])
+# run the function 
+print("Calculating abundance...")
+sps_ab = fns.calc_abundance_lines(abundance_containers, [sps_fc_filt])
+sps_ab = sps_ab.drop('fc_name', axis=1)
+print("Abundance esults: ")
+print(sps_ab.head())
 
 # combine 
+print("Combining results...")
 sps_result = pd.merge(sps_pres, sps_ab, how="left", on=["SITE_CODE"])
+print(sps_result.head())
 
 # check that field is unique
 def check_key(df, key_column):
@@ -80,7 +78,7 @@ def check_key(df, key_column):
 check_key(sps_result, 'SITE_CODE')
 
 # calculate cps presence and abundance -----------------------------------
-
+print("On to CPS now...")
 # concatenate cps SITE_NO and REGION cols into SITE_CODE
 cps_df['SITE_NO_str'] = cps_df['SITE_NO'].astype(str)
 
@@ -101,38 +99,19 @@ cps_pres = cps_df.groupby('SITE_CODE', as_index=False).agg(
 cps_pres['year'] = '2019'
 cps_pres['source'] = 'WADNR_cps_boat_survey'
 
-# calculate total_length for each SITE_CODE
-cps_df['total_length'] = cps_df.groupby('SITE_CODE')['LENGTH'].transform('sum')
+# abundance
+# create a filtered version of the dataset with only line segments w/ kelp present 
+cps_df_filt = cps_df[cps_df['kelp']== 1]
+cps_fc_filt = "scratch.gdb\\cps_kelp_only"
+cps_df_filt.spatial.to_featureclass(location=cps_fc_filt, overwrite=True)
 
-# calculate weight of each section
-cps_df['weight'] = cps_df['LENGTH'] / cps_df['total_length']
-
-# use presence to convert forest presence to 1 and incidental presence to 0.5
-# in other words, under weight incidental presence
-cps_df['kelp_qual'] = cps_df['type'].map({
-    'i': 0.5,
-    'f': 1,
-    'a': 0
-})
-
-# get weighted presence for each section
-cps_df['w_pres'] = cps_df['weight'] * cps_df['kelp_qual']
-
-# sum weighed presence by SITE_CODE
-cps_ab = (cps_df.groupby('SITE_CODE')
-                .agg(sum_w_pres=('w_pres', 'sum'))
-                .reset_index())
-
-# convert to abundance
-cps_ab['abundance'] = pd.cut(cps_ab['sum_w_pres'],
-                                    bins=[-float('inf'), 0, 0.25, 0.5, 0.75, float('inf')],
-                                    labels=[0, 1, 2, 3, 4])
-
-# drop extra col
-cps_ab = cps_ab.drop(columns=['sum_w_pres'])
+# run the function 
+cps_ab = fns.calc_abundance_lines(abundance_containers, [cps_fc_filt])
+cps_ab = cps_ab.drop('fc_name', axis=1)
 
 # combine 
 cps_result = pd.merge(cps_pres, cps_ab, how="left", on=["SITE_CODE"])
+
 # check that site_code is unique 
 check_key(cps_result, 'SITE_CODE')
 
@@ -144,3 +123,5 @@ print(cps_result.head())
 # save to csv in results folder
 sps_result.to_csv("kelp_data_synth_results\\sps_2017.csv")
 cps_result.to_csv("kelp_data_synth_results\\cps_2019.csv")
+
+fns.clear_scratch()
