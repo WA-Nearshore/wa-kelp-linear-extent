58c43456404b9aea380766b1499492d70a2bddcc
graymckenna96 <graymckenna96@gmail.com>
2025-04-08 15:02:14 -0700

    rescript the presence and abundance calculations to try to be more in alignment with other data source analyses

--- shorezone_synth.py
+++ shorezone_synth.py
@@ -4,8 +4,7 @@
 
 # Data downloaded 2024-07-30 from: 
 # 20240730 https://fortress.wa.gov/dnr/adminsa/gisdata/datadownload/state_DNR_ShoreZone.zip
-
-# note to self -- output format needs some tidying
+# minor manual preprocessing -> see note about years below
 
 # set env -----------------------------------------------
 import arcpy
@@ -14,14 +13,15 @@ import pandas as pd
 import numpy as np
 from arcgis.features import GeoAccessor, GeoSeriesAccessor
 import matplotlib.pyplot as plt
+import sys
+sys.path.append(os.getcwd())
+
+# import the project function library 
+import fns
 
 arcpy.env.overwriteOutput = True
 
-# store parent folder workspace in function 
-def reset_ws(): 
-    arcpy.env.workspace = os.getcwd()
-
-reset_ws()
+fns.reset_ws()
 
 # prep data ---------------------------------------------
 
@@ -37,16 +37,16 @@ kelp_lines = "kelp_data_sources\\state_DNR_ShoreZone\\shorezone_themes.gdb\\fkel
 svy_lines = "kelp_data_sources\\state_DNR_ShoreZone\\shorezone.gdb\\szline" 
 # Just need VIDEO_DATE field from this to get the year 
 
-# convert lines to polygons ------------------------------
+# prepare data ------------------------------
 
 # buffer fkelplin by ~100m
 print("Buffering lines...")
-buff_lines = "scratch.gdb\\fkelplin_buff100m"
-arcpy.analysis.Buffer(kelp_lines, buff_lines, '100 METERS')
+buff_lines = "scratch.gdb\\fkelplin_buff10m"
+arcpy.analysis.Buffer(kelp_lines, buff_lines, '10 METERS')
 
 # Remove overlaps 
 print("Removing overlaps...")
-buff_lines_RO = "scratch.gdb\\fkelplin_buf100m_removeO"
+buff_lines_RO = "scratch.gdb\\fkelplin_buf10m_removeO"
 arcpy.analysis.RemoveOverlapMultiple(
     in_features=buff_lines,
     out_feature_class=buff_lines_RO,
@@ -74,6 +74,8 @@ print("Getting year attribute...")
 buff_line_join = arcpy.management.AddJoin(buff_lines_RO, 'UNIT_ID', svy_lines, 'UNIT_ID')
 
 # grab year from the date field 
+# Note --> for a 42 of the szline features, video date is 0
+# manually calculated those missing values from the BIO_MAP_DT field in ArcGIS Pro before running this
 buff_line_year = arcpy.management.CalculateField(
     in_table = buff_line_join, 
     field = 'year',
@@ -86,7 +88,16 @@ buff_line_year = arcpy.management.CalculateField(
 # Remove join
 buff_line = arcpy.management.RemoveJoin(buff_line_join)
 
-# calculate presence --------------------------------------------
+print("Creating a version of the data with kelp features only...")
+# filter feature class to only kelp presence features
+buff_kelp_only = "scratch.gdb\\buff_kelp_only"
+arcpy.management.CopyFeatures(buff_line, buff_kelp_only)
+with arcpy.da.UpdateCursor(buff_kelp_only, ["FLOATKELP","OID@"]) as cursor:
+    for row in cursor:
+        if row[0] == 'ABSENT':
+            cursor.deleteRow()
+
+# select appropriate year for each segment ----------------------
 
 # Run an intersect
 print("Intersecting shorezone data with containers...")
@@ -107,51 +118,59 @@ arcpy.management.CalculateField(
 )
 
 # Export the intersect table to pd dataframe
-print("Exporting to dataframe...")
 df = pd.DataFrame.spatial.from_featureclass(kelp_int)
 df['area'] = pd.to_numeric(df['area'])
 df['area'] = df['area'].fillna(0)
 
-# calculate abundance -----------------------------------------
-print("Calculating abundance..")
-# Compute abundance category per site
-sum_area = (df.groupby(['SITE_CODE', 'FLOATKELP'])
-            .agg(total_area=('area', 'sum'))
-            .reset_index()
-            .pivot_table(index='SITE_CODE', columns='FLOATKELP', values='total_area', aggfunc='sum')
-            .fillna(0))
+print("Selecting year for each segment...")
 
-# Calculate presence percentage and abundance category
-sum_area['pres_pct'] = (sum_area['CONTINUOUS'] + sum_area['PATCHY'] * 0.5) / (sum_area['CONTINUOUS'] + sum_area['PATCHY'] + sum_area['ABSENT'])
-sum_area['abundance'] = pd.cut(sum_area['pres_pct'], 
-                                   bins=[-np.inf, 0, 0.25, 0.5, 0.75, np.inf], 
-                                   labels=[0, 1, 2, 3, 4])
-
-# Presence category (binary: 1 if presence > 0, else 0)
-sum_area['presence'] = np.where(sum_area['pres_pct'] > 0, 1, 0)
-
-print("Tidying results...")
-# Select year for each site with the maximum area
+# Select year for each site with the maximum area in container
 site_year = (df.groupby(['SITE_CODE', 'year'])
-             .agg(year_area=('area', 'sum'))
+             .agg(year_area=('area', 'sum')) 
              .reset_index())
 
-# Now, select the row with the maximum area per site
 site_year_max = site_year.loc[site_year.groupby('SITE_CODE')['year_area'].idxmax()]
 
 # Check if values are unique
-print(f"Unique SITE_CODEs in site_year: {site_year['SITE_CODE'].nunique()}")
-print(f"Unique SITE_CODEs in sum_area: {sum_area.index.nunique()}")
+print("Year dataframe:")
+print(site_year_max.head())
+print(site_year_max.info())
 
+# calculate presence --------------------------------------------
+
+fns.sum_kelp_within([buff_kelp_only], containers)
+
+presence = fns.df_from_fc(["scratch.gdb\\sumwithinbuff_kelp_only"], "WADNR_ShoreZone")
+presence = pd.concat(presence)
+print("Presence result:")
+print(presence.head())
+print(presence.info())
+
+# calculate abundance -----------------------------------------
+
+abundance_containers = "LinearExtent.gdb\\abundance_containers"
+abundance = fns.calc_abundance(abundance_containers, [buff_kelp_only])
+print("Abundance result:")
+print(abundance.head())
+print(abundance.info())
 # compile and export ---------------------------------------
-# Join the results on SITE_CODE
-result = pd.merge(sum_area, site_year, on='SITE_CODE')
+
+print("Compiling presence and abundance...")
+result = pd.merge(presence, abundance, how='left', on = 'SITE_CODE')
+print(result.head())
+print(result.info())
+print("Adding year column...")
+result = pd.merge(result, site_year_max, how='left', on = 'SITE_CODE')
+print(result.head())
+print(result.info())
 
 # reformat table 
-result = result[['SITE_CODE', 'abundance', 'presence', 'year']]
-result['source'] = 'WADNR_ShoreZone'
-print("Results table:")
+result['year'] = result['year_y']
+result = result[['SITE_CODE', 'abundance', 'presence', 'year', 'source']]
+print("Final results table:")
+print(result.info())
 print(result.head())
 
 # Write the result to CSV
 result.to_csv(r"kelp_data_synth_results\shorezone_synth.csv")
+print("Fin.")
