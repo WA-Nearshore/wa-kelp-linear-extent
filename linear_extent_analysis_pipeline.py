9870c089fde89496229b5857538c22bf73df9142
graymckenna96 <graymckenna96@gmail.com>
2025-05-30 15:10:10 -0700

    add some annotations

--- linear_extent_analysis_pipeline.py
+++ linear_extent_analysis_pipeline.py
@@ -9,19 +9,20 @@ import subprocess
 subprocess.run(["python", "COSTR_synth.py"])
 subprocess.run(["python", "AQRES_synth.py"])
 subprocess.run(["python", "cps_sps_reformat.py"])
-#subprocess.run(["python", "samish_synth.py"])
 subprocess.run(["python", "cps_uas_synth.py"])
 subprocess.run(["python", "shorezone_synth.py"])
+
 
 # Living datasources
 # Rerun as updates occur 
 subprocess.run(["python", "DNR_kayak_synth.py"]) # Annual updates anticipated
 subprocess.run(["python", "fixedwing_poly_synth.py"]) # Annual updates anticipated
-subprocess.run(["python", "mrc_kayak_synth.py"])
+subprocess.run(["python", "mrc_kayak_synth.py"]) # Annual updates anticipated
+subprocess.run(["python", "samish_synth.py"]) # Not sure update schedule
 
 
 # Run the join script
-#subprocess.run(["python", "kelp_synth_join.py"])
+subprocess.run(["python", "kelp_synth_join.py"])
 
 # Linear Extent updated
 print("Analysis complete")
