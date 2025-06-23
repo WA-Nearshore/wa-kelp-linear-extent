b4cfd42247c63df2645d0ee652a67b044db4eda9
graymckenna96 <graymckenna96@gmail.com>
2025-06-23 16:18:11 -0700

    tidy script with Ruff

--- linear_extent_analysis_pipeline.py
+++ linear_extent_analysis_pipeline.py
@@ -1,22 +1,30 @@
-# Load modules 
+# Load modules
 
 import subprocess
 
 # All data sources should be copied into /kelp_data_sources folder
-# Check the notes at the top of each script for any file naming info or pre-processing 
+# Check the notes at the top of each script for any file naming info or pre-processing
+
 
 def run_script(py_file):
     subprocess.run(["python", py_file])
 
+
 # Historical/"one time" datasources, no updates anticipated
 # No need to rerun unless lines/containers/source datasets have been editted since 06/2025
 
-historical_sources = ["cps_sps_reformat.py", "cps_uas_synth.py", "shorezone_synth.py", "seattle_1984_synth.py", "sps_historical_synth.py"]
+historical_sources = [
+    "cps_sps_reformat.py",
+    "cps_uas_synth.py",
+    "shorezone_synth.py",
+    "seattle_1984_synth.py",
+    "sps_historical_synth.py",
+]
 
 for script in historical_sources:
     print("----------------------------------")
     print(f"Running {script}...")
-    try: 
+    try:
         run_script(script)
         print(f"{script} complete")
         print("----------------------------------")
@@ -24,14 +32,20 @@ for script in historical_sources:
         print(f"Error occured while running {script}: {e}")
 
 # Living datasources
-# Rerun as updates occur 
-living_sources = ["COSTR_synth.py", "AQRES_synth.py", "DNR_kayak_synth.py", "fixedwing_poly_synth.py",
-                  "mrc_kayak_synth.py", "samish_synth.py"]
+# Rerun as updates occur
+living_sources = [
+    "COSTR_synth.py",
+    "AQRES_synth.py",
+    "DNR_kayak_synth.py",
+    "fixedwing_poly_synth.py",
+    "mrc_kayak_synth.py",
+    "samish_synth.py",
+]
 
 for script in living_sources:
     print("----------------------------------")
     print(f"Running {script}...")
-    try: 
+    try:
         run_script(script)
         print(f"{script} complete")
         print("----------------------------------")
