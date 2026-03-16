b273d592997427d924b72f90411fd4a54fa5166e
graymckenna96 <graymckenna96@gmail.com>
2026-03-16 14:35:24 -0700

    improve efficiency of coverage category function, change names to remove "abundance", add doc strings, improve handling of linear vs polygon kelp presence data

--- kelp_linear_extent/fns.py
+++ kelp_linear_extent/fns.py
@@ -51,10 +51,22 @@ def clear_scratch(SCRATCH_WS = os.path.join(os.path.dirname(os.path.dirname(os.p
 
 # main tools ------------------------------------------------------------------------------------
 # function to calculate presence
-def sum_kelp_within(fc_list, containers, SCRATCH_WS = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "scratch.gdb"), variable_survey_area=False): 
-    # fc_list = list of feature class of kelp beds OR paired list of kelp feature classes, kelp survey area
-    # containers = containers for summarize within ALREADY CLIPPED TO SURVEY EXTENT if variable_survey_area=False
-    
+def sum_kelp_within(fc_list, containers, SCRATCH_WS = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "scratch.gdb"), 
+                    variable_survey_area=False, kelp_geometry_type="polygon"): 
+    """
+    * **fc_list**: list of feature class of kelp beds OR paired list of kelp feature classes, kelp survey area if variable_survey_area=True
+    * **containers**: for summarize within ALREADY CLIPPED TO SURVEY EXTENT if variable_survey_area=False
+    * **SCRATCH_WS**: workspace for outputting sumwithin results; defaults to path from config_scratch
+    * **variable_survey_area**: defaults to FALSE if the same area was surveyed every year. Change to TRUE if any years had different survey area
+    * **kelp_geometry_type**: "polygon" [default] when kelp presence is polygons or "line" if kelp presence is lines
+    """
+    if kelp_geometry_type == "polygon":
+        unit = "HECTARES"
+    elif kelp_geometry_type == "line":
+        unit = "METERS"
+    else:
+        raise ValueError("kelp_geometry_type must be line or polygon")
+
     # intialize list of output fcs
     sumwithin_fcs = []
 
@@ -84,7 +96,7 @@ def sum_kelp_within(fc_list, containers, SCRATCH_WS = os.path.join(os.path.dirna
                     in_polygons = containers_clip,
                     in_sum_features = kelp_fc,
                     out_feature_class = out_fc, 
-                    shape_unit="HECTARES"
+                    shape_unit=unit
                 ) # save results in scratch gdb 
 
                 print("Summarize Within complete for " + fc_desc.name)
@@ -114,7 +126,7 @@ def sum_kelp_within(fc_list, containers, SCRATCH_WS = os.path.join(os.path.dirna
                     in_polygons = containers,
                     in_sum_features = fc,
                     out_feature_class = out_fc,
-                    shape_unit = "HECTARES"
+                    shape_unit = unit
                 )
 
                 print(f"Summarize Within complete for {fc_desc.name}")
@@ -129,55 +141,80 @@ def sum_kelp_within(fc_list, containers, SCRATCH_WS = os.path.join(os.path.dirna
     return sumwithin_fcs
                     
 # feature class to dataframe
-def df_from_fc(in_features, source_name):
+def df_from_fc(in_features, source_name, kelp_geometry_type="polygon"):
 
     """
     converts list of features into a list of formatted dataframes
     * **in_features**: list of feature classes to convert to dataframes 
     * **source_name**: string to be used as source name in table
+    * **kelp_geometry_type**: "polygon" [default] when kelp presence is polygons or "line" if kelp presence is lines
     * note: input features MUST have year as last 4 characters of name for this to work 
     """
+    if kelp_geometry_type == "polygon":
+        pres_col = "sum_Area_HECTARES"
+    elif kelp_geometry_type == "line":
+        pres_col = "sum_Length_METERS"
+    else:
+        raise ValueError("kelp_geometry_type must be line or polygon")
 
+    # if each year/survey needs its own survey area: 
     sdf_list = []
     for feature in in_features:
         
         fc_desc = arcpy.Describe(feature)
 
         sdf = pd.DataFrame.spatial.from_featureclass(feature) 
-        sdf = sdf.filter(['SITE_CODE', 'sum_Area_HECTARES'], axis = 1) #drop unneeded SHAPE cols
+
+        sdf = sdf.filter(['SITE_CODE', pres_col], axis = 1) #drop unneeded SHAPE cols
         sdf['year'] = fc_desc.name[-4:]
         sdf['source'] = source_name
-        sdf['presence'] = np.where(sdf['sum_Area_HECTARES'] > 0, 1, 0)
+        sdf['presence'] = np.where(sdf[pres_col] > 0, 1, 0)
 
         sdf_list.append(sdf)
-
         print("Converted " + fc_desc.name + " to sdf and added to list")
 
     return sdf_list    
 
-# tool for calculating proportional presence (abundance) of polygon kelp beds along line segments
-def calc_abundance(abundance_containers, kelp_fcs, PROJECT_ROOT):
-    
+# tool for calculating coverage category of polygon kelp beds along line segments
+def calc_cov_cat(cov_cat_containers, kelp_fcs, SCRATCH_WS = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "scratch.gdb"), 
+                                                                         kelp_geometry_type = "polygon"):
+    """
+    Calculates coverage category for polygon kelp presence features 
+    * **cov_cat_containers**: feature class with the subdivided containers
+    * **kelp_fcs**: list of feature classes with kelp presence polygons to be analyzed
+    * **kelp_geometry_type**: "polygon" [default] when kelp presence is polygons or "line" if kelp presence is lines
+    * note, lines must be ONLY presence lines (filter out absence lines upstream)
+    * **PROJECT_ROOT**: path to the parent folder 
+    """
     #initial result sdf list 
     df_list = []
+    
+    if kelp_geometry_type == "polygon":
+        unit = "HECTARES"
+        pres_col = "sum_Area_HECTARES"
+    elif kelp_geometry_type == "line":
+        unit = "METERS"
+        pres_col = "sum_Length_METERS"
 
-    # summarize within --> do NOT clip abundance containers to survey area, 
+
+    # summarize within --> do NOT clip cov cat containers to survey area, 
     for fc in kelp_fcs:
 
         # get the describe object for the feature class
         fc_desc = arcpy.Describe(fc)
 
         # set the out path for the analyzed feature classes 
-        out_fc = os.path.join(PROJECT_ROOT, "scratch.gdb", f"ab{fc_desc.name}")
+        out_fc = os.path.join(SCRATCH_WS, f"ab{fc_desc.name}")
 
         # run summarize within
         try: 
-            print(f"Running Abundance SummarizeWithin for {fc_desc.name}...")
+            print(f"Running Coverage Category SummarizeWithin for {fc_desc.name}...")
+            print(f"Results will be written to {out_fc}")
             arcpy.analysis.SummarizeWithin(
-                in_polygons = abundance_containers,
+                in_polygons = cov_cat_containers,
                 in_sum_features = fc,
                 out_feature_class = out_fc,
-                shape_unit = "HECTARES"
+                shape_unit = unit
             )
             print(f"Result written to {out_fc}")
         except arcpy.ExecuteError:
@@ -197,7 +234,7 @@ def calc_abundance(abundance_containers, kelp_fcs, PROJECT_ROOT):
         df['weight'] = df['length_m'] / df['total_length']
 
         # calculate presence
-        df['presence'] = df['sum_Area_HECTARES'].apply(lambda x: 1 if x > 0 else 0)
+        df['presence'] = df[pres_col].apply(lambda x: 1 if x > 0 else 0)
 
         # get weighted presence for each section
         df['w_pres'] = df['weight'] * df['presence']
@@ -207,14 +244,14 @@ def calc_abundance(abundance_containers, kelp_fcs, PROJECT_ROOT):
                 .agg(sum_w_pres=('w_pres', 'sum'))
                 .reset_index())
 
-        # categorize abundance based on weighted presence 
-        print("Calculating abundance...")
-        result['abundance'] = pd.cut(result['sum_w_pres'],
+        # categorize cov cat based on weighted presence 
+        print("Calculating coverage category...")
+        result['coverage_cat'] = pd.cut(result['sum_w_pres'],
                                     bins=[-float('inf'), 0, 0.25, 0.5, 0.75, float('inf')],
                                     labels=[0, 1, 2, 3, 4])
         
         # keep only relevant columns
-        result = result[['SITE_CODE', 'abundance']]
+        result = result[['SITE_CODE', 'coverage_cat']]
         
         # add a column with name of fc input. will need to be uniquely reformatted per data source to get year 
         result['fc_name'] = str(fc_desc.name)
@@ -229,81 +266,5 @@ def calc_abundance(abundance_containers, kelp_fcs, PROJECT_ROOT):
         # append result to df list
         df_list.append(result)
 
-    abundance_results = pd.concat(df_list)
-    return abundance_results
-
-# create a function to calculate abundance (aka proportional presence) for line-based datasets
-# needs to run on a copy of line based datasets that have been filtered to ONLY kelp presence features
-def calc_abundance_lines(abundance_containers, kelp_fcs, PROJECT_ROOT):
-    
-    #initial result sdf list 
-    df_list = []
-
-    # summarize within --> do NOT clip abundance containers to survey area, 
-    for fc in kelp_fcs:
-
-        # get the describe object for the feature class
-        fc_desc = arcpy.Describe(fc)
-
-        # set the out path for analyzed fcs
-        out_fc = os.path.join(PROJECT_ROOT, "scratch.gdb", f"ab_{fc_desc.name}")
-
-        # run summarize within
-        try: 
-            print(f"Running Abundance SummarizeWithin for {fc_desc.name}...")
-            arcpy.analysis.SummarizeWithin(
-                in_polygons = abundance_containers,
-                in_sum_features = fc,
-                out_feature_class = out_fc,
-                shape_unit = "METERS"
-            )
-            print(f"Result written to {out_fc}")
-        except arcpy.ExecuteError:
-            arcpy.AddError(arcpy.GetMessages(2))
-        except Exception as e:
-            print(e.args[0])
-
-        print("Converting to df...")
-        df = pd.DataFrame.spatial.from_featureclass(out_fc)
-
-        # calculate total_length for each SITE_CODE
-        df['total_length'] = df.groupby('SITE_CODE')['length_m'].transform('sum')
-
-        # calculate weight of each subdivided section based on original feature length
-        df['weight'] = df['length_m'] / df['total_length']
-
-        # calculate presence by returning 1 if any kelp presence in that segment 
-        df['presence'] = df['sum_Length_METERS'].apply(lambda x: 1 if x > 0 else 0)
-
-        # get weighted presence for each section
-        df['w_pres'] = df['weight'] * df['presence']
-
-        # sum weighted presence across site codes
-        result = (df.groupby('SITE_CODE')
-                .agg(sum_w_pres=('w_pres', 'sum'))
-                .reset_index())
-
-        # categorize abundance based on weighted presence 
-        print("Calculating abundance...")
-        result['abundance'] = pd.cut(result['sum_w_pres'],
-                                    bins=[-float('inf'), 0, 0.25, 0.5, 0.75, float('inf')],
-                                    labels=[0, 1, 2, 3, 4])
-        
-        # keep only relevant columns
-        result = result[['SITE_CODE', 'abundance']]
-        
-        # add a column with name of fc input. will need to be uniquely reformatted per data source to get year 
-        result['fc_name'] = str(fc_desc.name)
-
-        # view result
-        print("Abundance result:")
-        print(result.head())
-
-        # delete the feature class from memory
-        arcpy.management.Delete(out_fc)
-
-        # append result to df list
-        df_list.append(result)
-
-    abundance_results = pd.concat(df_list)
-    return abundance_results
+    cov_cat_results = pd.concat(df_list)
+    return cov_cat_results
