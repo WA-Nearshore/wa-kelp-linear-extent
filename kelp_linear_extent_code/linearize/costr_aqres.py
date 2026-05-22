1d3c21b1e609267d4910259517c56b468265b7c2
graymckenna96 <graymckenna96@gmail.com>
2026-05-22 07:59:45 -0700

    update to 2024 (combined COSTR/AQRES data) and new functions

--- kelp_linear_extent_code/linearize/costr_aqres.py
+++ kelp_linear_extent_code/linearize/costr_aqres.py
@@ -3,6 +3,7 @@
 # Need to update for 2026 now that these datasets are combined in a single gdb
 # current version from "..\kelp\projects\2025_COSTR_AQRES_1989_2024\data_download\WA_floating_kelp_coast_strait_reserves.gdb"
 # accessed 2026-02-19
+# Manually create two survey boundary fcs - only COSTR Map Indexes til 2011, then COSTR+AQRES Map Indexes 2011 onward
 
 # set up environment --------------------------------------
 
@@ -10,7 +11,7 @@ import sys
 import os
 import arcpy
 import pandas as pd
-from arcgis.features import GeoAccessor, GeoSeriesAccessor # these are used to create sedfs
+from arcgis.features import GeoAccessor, GeoSeriesAccessor # noqa: F401 # these are used to create sedfs
 
 # project root is the folder within which the entire kelp_linear_extent module is located (2 levels up from this file)
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -18,73 +19,90 @@ print("Project working directory:")
 print(PROJECT_ROOT)
 sys.path.append(PROJECT_ROOT) # this lets the project function library be found as a module
 
-import kelp_linear_extent_code.fns as fns # project function library
+import kelp_linear_extent_code.fns as fns # noqa: E402 project function library
 
 arcpy.env.overwriteOutput = True # overwrite outputs 
 
 # set workspace to parent folder
 fns.reset_ws()
 
-# configure scratch
-fns.config_scratch()
+# set up scratch workspace
+SCRATCH_WS = fns.config_scratch()
 
 # USER INPUT ------------------------------------------------
 
 dataset_name = "WADNR_COSTR_AQRES"
-containers = os.path.join(PROJECT_ROOT, "LinearExtent.gdb", "kelp_containers_v2")
-kelp_data_path = os.path.join(PROJECT_ROOT, "kelp_data_sources\\kelp_canopy_aquatic_reserves_adjusted.gdb")
-abundance_containers = os.path.join(PROJECT_ROOT, "LinearExtent.gdb\\abundance_containers")
+kelp_data_path = os.path.join(PROJECT_ROOT, "kelp_data_sources\\WA_floating_kelp_coast_strait_reserves.gdb")
+containers = os.path.join(PROJECT_ROOT, "LinearExtent.gdb\\lines_and_containers\\kelp_containers_v3")
+cov_cat_containers = os.path.join(PROJECT_ROOT, "LinearExtent.gdb\\lines_and_containers\\cov_cat_containers")
 
 # prep data ------------------------------------------------
 
 print(f"Using {containers} as container features")
-arcpy.env.workspace = kelp_data_path
+arcpy.env.workspace = f"{kelp_data_path}\\annual_data"
 
-# list AQRES feature classes
-aqres_fcs = arcpy.ListFeatureClasses("kelp1*")  # add all fcs from 2010s
-aqres_fcs.extend(arcpy.ListFeatureClasses("kelp2*"))  # add all fcs from 2020s
+# list feature classes
+kelp_fc_names = arcpy.ListFeatureClasses()  
 
 print("Datasets to be linearized:")
-for fc in aqres_fcs:
+for fc in kelp_fc_names:
     print(f"{fc}")
 
 # append path to fcs in list
-aqres_fcs = [f"{kelp_data_path}\\{fc}" for fc in aqres_fcs]
+kelp_fcs = [f"{kelp_data_path}\\annual_data\\{fc}" for fc in kelp_fc_names]
+
+# ensure list is sorted first year - last year
+kelp_fcs = sorted(kelp_fcs, key=lambda x: int(x[-4:]))
+print(kelp_fcs)
 
 # reset workspace to parent folder
 fns.reset_ws(PROJECT_ROOT)
 
-# clip containers to survey area
-aqres_bnd = os.path.join(PROJECT_ROOT, f"{kelp_data_path}\\map_index_ar")
-containers_clip = os.path.join(PROJECT_ROOT, "scratch.gdb\\containers_AQRES")
-arcpy.analysis.Clip(containers, aqres_bnd, containers_clip)
+# prepare survey boundaries --> split up map_index_polygons
+# need: 1989 only for 1989-2009, 1989 + 2010 for 2010, all for 2011+
 
-print("Container fc clipped to " + aqres_bnd.rsplit("\\", 1)[-1])
+print("Preparing survey boundaries...")
+# set up list
+svy_bnd = [f"{kelp_data_path}\\map_index_89_09", 
+           f"{kelp_data_path}\\map_index_10", 
+           f"{kelp_data_path}\\map_index_polygons"]
+
+if not arcpy.Exists(svy_bnd[2]):
+    raise Exception(f"WARNING: {svy_bnd} DOES NOT EXIST")
+
+if not arcpy.Exists(svy_bnd[0]):
+    print("Creating 89-09 survey boundary")
+    arcpy.conversion.ExportFeatures(svy_bnd[2],
+                                    svy_bnd[0],
+                                    '"first_yr" < 2010')
+
+if not arcpy.Exists(svy_bnd[1]):
+    print("Creating 2010 survey boundary")
+    arcpy.conversion.ExportFeatures(svy_bnd[2],
+                                    svy_bnd[1],
+                                    '"first_yr" < 2011')
+
+print("Survey boundaries: ")
+for fc in svy_bnd: 
+    print(fc)
+
+# combine lists 
+paired_fc_list = [
+    (a, svy_bnd[0]) if i < 20
+    else (a, svy_bnd[1]) if i == 20
+    else (a, svy_bnd[2])
+    for i, a in enumerate(kelp_fcs)
+]
+
+for pair in paired_fc_list: 
+    print(pair)
 
 # calculate presence  -----------------------------------------
-
-sumwithin_fcs = fns.sum_kelp_within(aqres_fcs, containers_clip, PROJECT_ROOT) # this function also creates scratch.gdb if it does not exist
-print(sumwithin_fcs)
-
-# rename fcs to have year at end (AQRES-specific step)
-try:
-    for fc in sumwithin_fcs:
-        fc_desc = arcpy.Describe(fc)
-        new_name = f"sum20{str(fc_desc.name[-4:-2])}"
-        arcpy.management.Rename(fc, new_name)
-        print(f"{fc_desc.name} renamed to {new_name}")
-except Exception as e: 
-    print(print(f"Unable to rename feature classes: {e}"))
-
-# reset list
-arcpy.env.workspace = os.path.join(PROJECT_ROOT, "scratch.gdb")
-sumwithin_fcs = arcpy.ListFeatureClasses("sum*")
-sumwithin_fcs = [os.path.join(PROJECT_ROOT, f"scratch.gdb\\{fc}") for fc in sumwithin_fcs]
-print(f"Reset feature class list: {sumwithin_fcs}")
-fns.reset_ws(PROJECT_ROOT)
+pres_fcs = fns.calc_presence(paired_fc_list, containers, variable_survey_area=True) # this function also creates scratch.gdb if it does not exist
+print(pres_fcs)
 
 # convert fcs to dfs
-sdf_list = fns.df_from_fc(in_features=sumwithin_fcs, source_name="WADNR_AQRES")
+sdf_list = fns.df_from_fc(in_features=pres_fcs, source_name=dataset_name)
 
 print("This is the structure of the sdfs:")
 print(sdf_list[1].head())
@@ -95,25 +113,26 @@ print("Presence data merged to one df")
 print(presence.head())
 
 # calculate abundance ---------------------------------------
+print("Calculating coverage category....")
 
+cov_cat = fns.calc_cov_cat(cov_cat_containers, kelp_fcs)
 
-abundance = fns.calc_abundance(abundance_containers, aqres_fcs)
-
-abundance["year"] = "20" + abundance["fc_name"].str[4:6]
-abundance = abundance.drop(columns=["fc_name"])
-print("Compiled abundance results:")
-print(abundance.head())
+cov_cat["year"] = cov_cat["fc_name"].str[-4:]
+cov_cat = cov_cat.drop(columns=["fc_name"])
+print("Compiled cov cat results:")
+print(cov_cat.head())
 
 # combine results -------------------------------------------
 
 print("Combining results to one dataframe")
-results = pd.merge(presence, abundance, how="left", on=["SITE_CODE", "year"])
+results = pd.merge(presence, cov_cat, how="left", on=["SITE_CODE", "year"])
 print(results.head())
 
 # Write to csv
-out_result = "AQRES_synth.csv"
-results.to_csv(os.path.join(PROJECT_ROOT, f"kelp_data_linear_outputs\\{out_result}"))
-print(f"Saved as csv here: kelp_linear_outputs\\{out_result}")
+os.makedirs(f"{PROJECT_ROOT}\\kelp_data_linear_outputs", exist_ok=True)
+out_results = os.path.join(PROJECT_ROOT, f"kelp_data_linear_outputs\\{dataset_name}_result.csv")
+results.to_csv(out_results)
+print(f"Saved as csv here: {out_results}")
 
 # Clear scratch gdb to keep project size down
 fns.clear_scratch()
